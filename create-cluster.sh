65ab07bd109ed29b731fbcd9592ea95f46151277
Max Jonas Werner <mwerner@d2iq.com>
2021-11-05 14:46:22 +0100

    add GitRepository manifest

--- create-cluster.sh
+++ create-cluster.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 kind create cluster
 flux install
-flux create source git flux-mc --url=https://github.com/makkes/flux-mc --branch=main --interval=1m
-flux create kustomization infra --source=GitRepository/flux-mc --path=infra
+kubectl apply -f infra/flux-mc.yaml
+kubectl apply -f infra/infra.yaml
